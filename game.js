99130c05f979446f1eaf3cb4417fc1a05ab08535
Aaron McLeod <sircoolguy99@gmail.com>
2014-06-19 23:54:04 -0400

    added melonjs loader

--- game.js
+++ game.js
@@ -1,5 +1,12 @@
 var Game = {
   init: function () {
+    me.loader.onload = this.startGame.bind(this);
+    me.loader.preload([{
+      type: "image", name: "mobileplayer", src: "data/mobileplayer.png"
+    }]);
+  },
+
+  startGame: function () {
     WebGLRenderer.init();
     this.sprite = new Sprite();
     WebGLRenderer.addChild(this.sprite);
